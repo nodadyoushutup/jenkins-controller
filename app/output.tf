d70838448e8611ac81151a479531b35cd429e81c
nodadyoushutup <admin@nodadyoushutup.com>
2025-09-23 21:02:11 +0000

    docker compose

--- app/output.tf
+++ app/output.tf
@@ -7,3 +7,7 @@ output "wait_for_service_id" {
   description = "ID of the null resource waiting for the controller service"
   value       = null_resource.wait_for_service.id
 }
+
+output "casc_config" {
+  value = yamlencode(var.casc_config)
+}
